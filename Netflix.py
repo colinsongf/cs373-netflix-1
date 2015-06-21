114246206478e0464d2b4d8e86eec98bc77d4263
Luis Lopez <lclg21@utexas.edu>
2015-06-21 17:46:47 -0500

    wrote functions and tests for reading and printing

--- Netflix.py
+++ Netflix.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict 
+
 # ------------
 # netflix_read
 # ------------
 
-def netflix_read (s) :
+def netflix_read (input) :
     """
     read two ints
-    s a string
+    input a string
     return a list of two ints, representing the beginning and end of a range, [i, j]
     """
-    a = s.split()
-    return [int(a[0]), int(a[1])]
+    to_predict_dict = OrderedDict()
+    a = input.split()
+    customer_id = -1
+    for s in a:
+        if s[(len(s) - 1):] == ':' :
+            try:
+                customer_id = int(s[0:len(s)-1])
+            except:
+                raise 
+
+            to_predict_dict[customer_id] = []
+
+        else:
+            try:
+                movie_id  = int(s)
+            except:
+                raise
+
+            to_predict_dict[customer_id].append(movie_id)
+
+    return to_predict_dict 
+
+
 
 # ------------
 # netflix_eval
@@ -30,7 +53,7 @@ def netflix_eval (i, j) :
 # netflix_print
 # -------------
 
-def netflix_print (w, i, j, v) :
+def netflix_print (w, predictions_dict) :
     """
     print three ints
     w a writer
@@ -38,7 +61,11 @@ def netflix_print (w, i, j, v) :
     j the end       of the range, inclusive
     v the max cycle length
     """
-    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+    
+    for key in predictions_dict:
+        w.write(str(key) + ':\n')
+        for value in predictions_dict[key]:
+            w.write(str(value) + '\n')
 
 # -------------
 # netflix_solve
@@ -49,7 +76,9 @@ def netflix_solve (r, w) :
     r a reader
     w a writer
     """
+    '''
     for s in r :
         i, j = netflix_read(s)
         v    = netflix_eval(i, j)
         netflix_print(w, i, j, v)
+    '''
--- TestNetflix.py
+++ TestNetflix.py
@@ -9,6 +9,7 @@
 from io       import StringIO
 from unittest import main, TestCase
 
+from collections import OrderedDict 
 from Netflix import netflix_read, netflix_eval, netflix_print, netflix_solve
 
 # -----------
@@ -20,11 +21,30 @@ class TestNetflix (TestCase) :
     # read
     # ----
 
-    def test_read (self) :
-        s    = "1 10\n"
-        i, j = netflix_read(s)
-        self.assertEqual(i,  1)
-        self.assertEqual(j, 10)
+    def test_read_1 (self) :
+        s    = "2043:\n1417435\n"
+        to_predict = netflix_read(s)
+        self.assertEqual(1, len(to_predict))
+        self.assertEqual(1417435, (to_predict[2043])[0])
+
+    def test_read_2 (self) :
+        s    = "2043:\n1417435\n2312054\n462685\n"
+        to_predict = netflix_read(s)
+        self.assertEqual(1, len(to_predict))
+        self.assertEqual(1417435, (to_predict[2043])[0])
+        self.assertEqual(2312054, (to_predict[2043])[1])
+        self.assertEqual(462685, (to_predict[2043])[2])
+
+    def test_read_2 (self) :
+        s    = "2043:\n1417435\n2312054\n462685\n10851:\n1417435\n1234567\n"
+        to_predict = netflix_read(s)
+        self.assertEqual(2, len(to_predict))
+        self.assertEqual(1417435, (to_predict[2043])[0])
+        self.assertEqual(2312054, (to_predict[2043])[1])
+        self.assertEqual(462685, (to_predict[2043])[2])
+        self.assertEqual(1417435, (to_predict[10851])[0])
+        self.assertEqual(1234567, (to_predict[10851])[1])
+
 
     # ----
     # eval
@@ -50,21 +70,28 @@ class TestNetflix (TestCase) :
     # print
     # -----
 
-    def test_print (self) :
+    def test_print_1 (self) :
         w = StringIO()
-        netflix_print(w, 1, 10, 20)
-        self.assertEqual(w.getvalue(), "1 10 20\n")
+        predictions_dict = OrderedDict([(2043, [3.4, 4.1, 1.9])])
+        netflix_print(w, predictions_dict)
+        self.assertEqual(w.getvalue(), "2043:\n3.4\n4.1\n1.9\n")
+
+    def test_print_2 (self) :
+        w = StringIO()
+        predictions_dict = OrderedDict([(2043, [3.4, 4.1, 1.9]), (10851, [4.3, 1.4, 2.8])])
+        netflix_print(w, predictions_dict)
+        self.assertEqual(w.getvalue(), "2043:\n3.4\n4.1\n1.9\n10851:\n4.3\n1.4\n2.8\n")
 
     # -----
     # solve
     # -----
-
+    '''
     def test_solve (self) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         netflix_solve(r, w)
         self.assertEqual(w.getvalue(), "1 10 1\n100 200 1\n201 210 1\n900 1000 1\n")
-
+    '''
 # ----
 # main
 # ----
